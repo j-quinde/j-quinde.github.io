ec20616865b21df0bb205bb83d8809043a27031d
Josselyn Quinde <jqquindem@gmail.com>
2022-01-31 16:59:57 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 
-
+/*
 const user = 'Josselyn Quinde';
 const saludo = <h1>Hola, soy {user}</h1>;
 ReactDOM.render(saludo,document.getElementById('div_saludo'));
+*/
+
+function Saludar(props) {
+    return <h1>Hola, soy {props.name}</h1>
+}
+
+ReactDOM.render(<Saludar name="Josselyn Quinde" />,document.getElementById('div_saludo'));
