914860d992928a750c9c05f412e1ccd8b8c4718e
jossy <jqquindem@gmail.com>
2024-06-08 17:50:30 -0500

    refactor (main.js - pokedex v2): se almacena matriz de pokemons a una variable global y la carga de los cards es más rápido que la version anterior

--- pokedex-v2/js/main.js
+++ pokedex-v2/js/main.js
@@ -18,7 +18,81 @@ const fillZeros = (number) => {
     return newNumber;
 }
 
-const fetchPokemon = async (id) => {
+const startId = 1;
+const endId = 151;
+const offset = 1;
+const limit = 9;
+
+const fetchPokemon = () => {
+    const arrPokemons = [];
+    for (let i = startId; i <= endId; i++) {
+        const urlPokemon = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        const jsonPokemon = fetch(urlPokemon).then((result) => result.json());
+        arrPokemons.push(jsonPokemon);
+    }
+
+    Promise.all(arrPokemons).then((result) => {
+        dataPokemon = result.map((res) => ({
+            id: res.id,
+            name: res.name,
+            type: res.types,
+            img1: res.sprites.front_default,
+            img2: res.sprites.other.home.front_default
+        }))
+        cardPokemon(dataPokemon)
+    })
+}
+
+const cardPokemon = (pkm) => {
+    pkm.forEach(pokemon => {
+
+        const cardContainer = document.querySelector('#cardContainer');
+        let div_card = document.createElement('div');
+        div_card.className = 'card';
+
+        let card_body = document.createElement('div');
+        card_body.className = 'card-body';
+
+        let cardDataPkm = document.createElement('div');
+        cardDataPkm.className = 'data-pkm';
+
+        let namePkm = document.createElement('p');
+
+        let spanNumber = document.createElement('span');
+        spanNumber.className = 'number-pkm';
+        spanNumber.textContent = ' N°' + fillZeros(pokemon.id) + ' ';
+
+        let typePkm = document.createElement('span');
+        typePkm.className = 'type-pokemon';
+
+        let imgPkm = document.createElement('img');
+        imgPkm.className = 'sprite-pkm';
+
+        namePkm.textContent = upperFirstLetter(pokemon.name);
+
+        const types_pokemon = pokemon.type.map((types) => {
+            return `<span class="type-pokemon ${types.type.name}">${upperWord(types_es[types.type.name])}</span>`;
+        }).join(' ');
+
+        if (pokemon.img1) {
+            imgPkm.src = pokemon.img1;
+        } else {
+            imgPkm.src = pokemon.img2;
+        }
+
+        cardContainer.appendChild(div_card);
+        div_card.appendChild(card_body);
+        card_body.appendChild(cardDataPkm);
+        cardDataPkm.innerHTML = types_pokemon;
+        cardDataPkm.prepend(namePkm);
+        cardDataPkm.prepend(spanNumber);
+        card_body.appendChild(imgPkm);
+    });
+}
+
+fetchPokemon()
+
+/*const fetchPokemon = async (id) => {
     try {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
         const dataPkm = await res.json();
@@ -84,7 +158,7 @@ for (let i = 1; i <= 5; i++) {
         cardPokemon();
     });
 }
-
+*/
 
 
 
--- pokedex-v2/js/regions.js
+++ pokedex-v2/js/regions.js
@@ -9,3 +9,21 @@ const regions = {
     alola: { name: "alola", startId: 722, endId: 809 },
     galar: { name: "galar", startId: 810, endId: 905}
 };
+
+/*
+const fetchRegion = async () => {
+    const urlRegion = "https://pokeapi.co/api/v2/region";
+    const jsonRegion = await fetch(urlRegion).then((res)=> res.json());
+    const countRegion = jsonRegion.count;
+    for (let i = 1; i <= countRegion; i++){
+        const urlDataRegion = `https://pokeapi.co/api/v2/pokedex/${i}/`;    
+        const jsonDataUrl = await fetch(urlDataRegion).then((res)=> res.json());
+        //const pokedexUrl = jsonDataUrl.pokedexes[0].url;
+        console.log(jsonDataUrl)
+
+    }
+    
+    console.log(countRegion)
+}
+
+fetchRegion()*/
