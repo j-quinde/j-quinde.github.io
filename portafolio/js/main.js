4f7fac6588526dd52cf3c59178221dc321babcfd
jossy <jqquindem@gmail.com>
2024-06-16 00:01:11 -0500

    add (portafolio): se agrega el .js y .css de la libreria Swiper para un carousel

--- portafolio/js/main.js
+++ portafolio/js/main.js
@@ -26,35 +26,6 @@ function OpenAlert() {
 
 ReactDOM.render(<OpenAlert />,document.getElementById('div_vermas'));
 */
-
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
   
 
 const openPokedex = () => {
